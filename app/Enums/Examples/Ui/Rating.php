5bfdc500bd2fecc504142072b383336d4e36237f
AJ Meireles <alvaro.meireles@live.com>
2024-04-29 15:03:11 -0300

    wip

--- app/Enums/Examples/Ui/Rating.php
+++ app/Enums/Examples/Ui/Rating.php
@@ -4,8 +4,39 @@ namespace App\Enums\Examples\Ui;
 
 class Rating
 {
+    public const PHP = <<<'HTML'
+    use Livewire\Component;
+    use Illuminate\Contracts\View\View;
+    
+    class Profile extends Component
+    {
+        public $rate = 2;
+    
+        public function render(): View
+        {
+            return view('livewire.profile');
+        }
+    
+        public function evaluate(int $quantity): void
+        {
+            //
+        }
+    }
+    HTML;
+
+    public const BLADE = <<<'HTML'
+    <!-- Manual -->
+    <x-rating :$rate />
+
+    <!-- Or binding using wire:model -->
+    <x-rating wire:model="rate" />
+    HTML;
+
     public const BASIC = <<<'HTML'
-    <x-rating />
+    <x-rating :$rate />
+
+    <!-- You can bind using wire:model -->
+    <x-rating wire:model="rate" />
     HTML;
 
     public const PERSONALIZATION = <<<'HTML'
--- resources/views/documentation/ui/rating.blade.php
+++ resources/views/documentation/ui/rating.blade.php
@@ -16,9 +16,15 @@
         <livewire:documentation.personalization :$personalization component="rating" />
     </x-slot:personalization>
     <x-section title="Persist Rating">
-        <x-preview language="blade" :contents="$basic">
-            //
-        </x-preview>
+        <p>
+            The logic for persisting the rating in a database, such as SQL, SQLite or Redis (cache)
+            is up to its own algorithm. <b>If, for example, you don't create logic, especially one that
+                blocks many rating from the same authenticated user, the user will rating unlimitedly.</b>
+            When a star is pressed, the <x-block>evaluate</x-block> method is triggered, receiving the
+            quantity of rating as a parameter. Let's take a look at an example:
+        </p>
+        <x-code :contents="$php" />
+        <x-code :contents="$blade" language="blade" />
     </x-section>
     <x-section title="Basic Usage">
         <x-preview language="blade" :contents="$basic">
