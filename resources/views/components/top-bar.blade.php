dd12fcabad46a0e2011d2510c59f42b1a8927143
AJ Meireles <alvaro.meireles@live.com>
2024-06-14 13:20:55 -0300

    wip

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,4 +1,5 @@
 <x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']"
           :until="now()->addWeek()">
-    <b class="underline">Version 1.30.0 is now available!</b> Introduced a useful command to search for component usage.
+    <b>New version 1.30.0 is now available</b> 🎉
+    <a class="underline" href="{{ route('documentation.command').'#find-component' }}">Introducing a useful command to search for component usage.</a>
 </x-banner>
