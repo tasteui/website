84afbdc690810aebc8227d94b123c5eae9d9401f
AJ Meireles <alvaro.meireles@live.com>
2024-07-11 11:36:50 -0300

    upgrading to v1.33.0

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,5 +1,7 @@
 <x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']"
           :until="now()->addDays(2)">
-    <b>Version 1.32.0 is here</b> 🎉
-    <a class="underline" href="{{ route('documentation.ui.button').'#normal-flat-variations' }}">Introduced Flat buttons, for normal and circle buttons.</a>
+    <b>Version 1.33.0 released</b> 🎉
+    <a href="https://github.com/tailwindlabs/heroicons/releases/tag/v2.1.5" target="_blank">
+        <u class="font-semibold">We've updated Heroicons to version 2.1.5</u>, which adds some new icons.
+    </a>
 </x-banner>
