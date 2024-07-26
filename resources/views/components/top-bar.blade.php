804f04fd0ca72aedb74a2aaac66f38da08a5ec56
AJ Meireles <alvaro.meireles@live.com>
2024-07-26 11:23:04 -0300

    removing top-bar warning

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,3 +1,3 @@
-<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-03-31">
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-03-20">
     The <a href="{{ route('documentation.summer-release') }}" class="underline">summer release is here</a> 🏖 A big release with lots of new features.
 </x-banner>
