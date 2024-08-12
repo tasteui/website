c9f5d8561a6b54812f7d3b138edfdf2390ce3232
AJ Meireles <alvaro.meireles@live.com>
2024-08-12 00:19:20 -0300

    wip

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,4 +1,4 @@
-<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" :until="now()->addWeek()">
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" :until="now()->subDay()">
     <b>Version 1.34.0 is here!</b> 🎉
     <a href="https://github.com/tallstackui/tallstackui/releases/tag/v1.34.0" target="_blank">
         <u class="font-semibold">Lots of new features and some fixes</u>
