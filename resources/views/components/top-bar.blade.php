cdab7fc65769fea423e9900077dbe8dc7a675846
AJ Meireles <alvaro.meireles@live.com>
2024-10-03 08:33:27 -0300

    v2 warning

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,6 +1,3 @@
-<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" :until="now()->subDay()">
-    <b>Version 1.34.0 is here!</b> 🎉
-    <a href="https://github.com/tallstackui/tallstackui/releases/tag/v1.34.0" target="_blank">
-        <u class="font-semibold">Lots of new features and some fixes</u>
-    </a>
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']">
+    <b class="underline">V2? Coming soon!</b> New components and lots of improvements.
 </x-banner>
