6d5c6a4fb79bbc10d8e66bad1e11514f0516f918
AJ Meireles <alvaro.meireles@live.com>
2025-01-16 10:05:16 -0300

    wip

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,3 +1,9 @@
+@if ($version === 'v2')
 <x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']">
-    <b class="underline">V2? Coming soon!</b> New components and lots of improvements.
+    <b class="underline">V2 is in public beta!</b> 🎉 Report any issues on the repository.
 </x-banner>
+@else
+    <x-banner :color="['background' => 'bg-red-600', 'text' => 'text-white']">
+        <b class="underline">WARNING!</b> You are browsing the docs for an old version of TallStackUI!
+    </x-banner>
+@endif
