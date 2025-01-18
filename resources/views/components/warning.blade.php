aa3cbf7eddd6d6b663b392714a82e196d604401d
AJ Meireles <alvaro.meireles@live.com>
2025-01-18 20:42:27 -0300

    wip

--- resources/views/components/warning.blade.php
+++ resources/views/components/warning.blade.php
@@ -3,7 +3,7 @@
 @php($text ??= $slot->toHtml())
 
 <div {{ $attributes }}>
-    <x-alert color="pink" icon="exclamation-circle" light>
+    <x-alert color="red" icon="exclamation-circle" light>
         {!! $text !!}
     </x-alert>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/docs', '/docs/v2/installation');
 Route::redirect('/install', '/docs/v2/installation');
 Route::redirect('/summer-release', '/docs/v1/summer-release');
+Route::redirect('/issue', 'https://github.com/tallstackui/tallstackui/issues/new?template=bug_report.yml')->name('issue');
 
 Route::middleware(ShareVersionVariable::class)
     ->group(function () {
